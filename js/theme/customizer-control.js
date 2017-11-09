61f85511b093d0f0a141a9e90021bf89f9a47848
DESKTOP-2OAO8F6\Rob <rob@fox.agency>
2017-11-09 10:19:23 +0000

    Cleaned up Google API warning trigger Added "Ready" listener on preview frame to API warning Added link to event listener for future help

--- js/theme/customizer-control.js
+++ js/theme/customizer-control.js
@@ -1,5 +1,11 @@
 (function( $ ) {
     
+    //Helpful link
+    //https://wpchat.com/t/i-came-across-this-not-so-flexible-customizer-js-api-code-in-almost-all-new-automattic-themes-that-allows-panel-based-content-is-there-a-known-bug/2250
+    
+    //Script to monitor events
+    //http://riceball.com/observing-the-wordpress-customizer-wp-customize-events/
+    
     wp.customize.bind( 'ready', function() {
         var customize = this;
         
@@ -38,35 +44,48 @@
         
         
         /**
+         * On first load, check if API warning is required
+         * Fires when the page firsts loads
+         */
+        
+        toggle_google_api_visibility();
+        
+        
+        /**
+         * On pane load, check if API warning in required
+         * Fired when everything has loaded
+         */
+        customize.previewer.bind( 'ready', function() {
+            toggle_google_api_visibility();
+        } );
+        
+                
+        /**
          * On Font panel expansion, check if the font API warning is required
+         * Fired when panel expands, to check for any changes
          */ 
         
-        wp.customize.section( 'indie_studio_fonts_colours_section' ).expanded.bind( function( isExpanding ) {
+        customize.section( 'indie_studio_fonts_colours_section' ).expanded.bind( function( isExpanding ) {
             if ( isExpanding ){
-                if ( customize( 'indie_studio_google_api' ).get().length > 0 ){
-                    toggle_google_api_visibility( false );
-                } else {
-                    toggle_google_api_visibility( true );
-                }
+                toggle_google_api_visibility();
             }
         } );
-                
-        function toggle_google_api_visibility( show ){
-            var google_font_warning = customize.control('indie_studio_font_api_missing');       
-            if ( show ){
+        
+        
+        /**
+         * Checks to see if there is a value in the Google API field.
+         * If there is, hide API warning, if not show API warning
+         */
+        
+        function toggle_google_api_visibility(){
+            var google_font_warning = customize.control('indie_studio_font_api_missing');   
+            if ( customize( 'indie_studio_google_api' ).get().length < 0 ){
                 google_font_warning.activate();
             } else {
                 google_font_warning.deactivate();
             }
         }
         
-        if ( customize( 'indie_studio_google_api' ).get().length > 0 ){
-            toggle_google_api_visibility( false );
-        } else {
-            toggle_google_api_visibility( true );
-        }
-        
-
         
     } );
     
