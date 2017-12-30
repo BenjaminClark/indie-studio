a39bad246baa2bbc1eb0ebc41cb55e1aca465a7a
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2017-12-30 13:49:21 +0000

    Added alternate search form for search page

--- inc/search.php
+++ inc/search.php
@@ -40,4 +40,28 @@ function customized_search_form( $form ) {
 	return $form;
     
 }
-add_filter( 'get_search_form', 'customized_search_form' );
+add_filter( 'get_search_form', 'customized_search_form' );#
+
+
+
+function main_search_form() {
+    
+    $form = '<form role="search" id="searchform" class="main-search" method="get" action="' . esc_url( home_url( '/' ) ) . '" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">';
+
+        $form .= '<label>';
+    
+        $form .= '<span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>';
+
+            $form .= '<input id="main-search-input" type="text" id="s" name="s" value="' . esc_attr( get_search_query() ) . '" placeholder="' .  esc_attr_x( 'Search …', 'placeholder' ) . '" title="' . esc_attr_x( 'Search for:', 'label' ) . '" itemprop="query-input" autocomplete="off">';
+
+        $form .= '</label>';
+    
+        $form .= '<button class="search-submit hover smooth"><i class="fa fa-search" aria-hidden="true"></i></button>';
+        
+        $form .= '<meta itemprop="target" content="' . site_url( '/?s={search} ' ) . '"/>';
+    
+    $form .= '</form>';    
+
+    echo $form;
+    
+}
--- search.php
+++ search.php
@@ -42,6 +42,7 @@ get_header(); ?>
 
                     <div class="entry-content e-entry-content">
                         <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', indie_studio_text_domain() ); ?></p>
+                        <?php main_search_form(); ?>
                     </div>
                 </article>
 
