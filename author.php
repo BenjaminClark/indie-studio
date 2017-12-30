be4bc98d898a3e20da441f194d8fb1a7fbf465ea
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2017-12-30 18:55:03 +0000

    Added author avatar and description

--- author.php
+++ author.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
         <div class="page-inner-wrap">
 
-        <?php if ( have_posts() ) : ?>
+        <?php if ( have_posts() ) { ?>
 
             <?php
                 /* Queue the first post, that way we know
@@ -25,12 +25,18 @@ get_header(); ?>
                 the_post();
             ?>
 
-            <header class="page-header author vcard h-card" itemprop="author" itemscope itemtype="http://schema.org/Person">
-                <h1 class="page-title"><?php printf( __( 'Author: %s', indie_studio_text_domain() ), '<a class="url u-url fn p-fn n p-name" href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me author" itemprop="url"><span itemprop="name">' . get_the_author() . '</span></a>' ); ?></h1>
+            <header class="page-header author vcard h-card group" itemprop="author" itemscope itemtype="http://schema.org/Person">
+                <h1 class="page-title"><?php printf( __( 'Articles by %s', indie_studio_text_domain() ), '<a class="url u-url fn p-fn n p-name" href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me author" itemprop="url"><span itemprop="name">' . get_the_author() . '</span></a>' ); ?></h1>
+                
+                
+                
                 <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
             <?php if ( get_the_author_meta( 'description' ) ) { ?>
-                <div class="author-note note p-note" itemprop="description"><p><?php echo get_the_author_meta( 'description' ); ?></p></div>
+                <div class="author-note note p-note wysiwyg" itemprop="description"><p><?php echo get_the_author_meta( 'description' ); ?></p></div>
             <?php } ?>
+                
+                
+                
             </header>
 
             <?php
@@ -41,35 +47,33 @@ get_header(); ?>
                 rewind_posts();
             ?>
 
-            <div id="ajax-post-wrap">
+                <div id="ajax-post-wrap" class="grid-container masonry basic">
+                    <div class="masonry-column-sizer"></div>
+                    <div class="masonry-gutter-sizer"></div>
+                    
+                    <?php /* Start the Loop */ ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php /* Start the Loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
+                        <?php
+                            /* Include the Post-Format-specific template for the content.
+                             * If you want to overload this in a child theme then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'template-parts/post/module', get_post_format() );
+                        ?>
 
-                    <?php get_template_part( 'template-parts/post/content', 'search' ); ?>
+                    <?php endwhile; ?>
 
-                <?php endwhile; ?>
-
-            </div>
-
-            <?php indie_studio_content_nav( 'nav-below', 'Load More' ); ?>
-
-        <?php else : ?>
-
-            <article id="post-0" class="post no-results not-found">
-
-                <header class="entry-header">
-                    <h1 class="entry-title p-entry-title"><?php _e( 'Nothing Found', indie_studio_text_domain() ); ?></h1>
-                </header><!-- .entry-header -->
-
-                <div class="entry-content e-entry-content">
-                    <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', indie_studio_text_domain() ); ?></p>
-                    <?php get_search_form(); ?>
                 </div>
 
-            </article>
+                <?php 
+                indie_studio_content_nav( 'nav-below', 'load-more' );
 
-        <?php endif; ?>
+            } else {
+
+                get_template_part( 'template-parts/post/content', 'none' );
+
+            } ?>
         
         </div>
 
