474ec81926cca0d6edac2827c2e11478a35de1ae
BenjaminClark <33034579+BenjaminClark@users.noreply.github.com>
2018-01-09 20:50:56 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
             <meta itemprop="name" content="<?php echo get_bloginfo( 'name', 'display' ); ?>" />
             <meta itemprop="url" content="<?php echo home_url( '/' ); ?>" />
 
-            <p class="copyright">Website &copy; <?php echo indie_studio_name() . ' ' . date('Y'); ?></p>
+            <p class="copyright"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">IndieStudio</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/BenjaminClark/indie-studio" property="cc:attributionName" rel="cc:attributionURL">Robert Marshall and Ben Clark</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://indieweb.org/" rel="dct:source">https://indieweb.org/</a>.</p>
             
             <p class="designer"><a href="http://www.justlikethis.co.uk">Created by JLT</a></p>
 
