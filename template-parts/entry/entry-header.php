7a4e5f2afff67e793954c301d16a89cee54c9ec9
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-05-12 16:38:12 +0100

    Reduced file size by removing duplicate code

--- template-parts/entry/entry-header.php
+++ template-parts/entry/entry-header.php
@@ -1,12 +1,23 @@
+<?php
+
+/**
+ * This creates the header section for all articles
+ * 
+ * If it is not a search item - show categories
+ **/ 
+
+?>
+
 <header class="entry-header">
    
-    <?php if ( ! is_search() ) { ?>
-   
-        <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', indie_studio_text_domain() ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
-    
-        <div class="entry-meta group">
+    <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', indie_studio_text_domain() ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+
+    <div class="entry-meta group">
+       
+        <?php if ( ! is_search() ) { ?>
+
             <?php indie_studio_posted_details(); ?>
-            
+
             <?php
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list( __( ', ', indie_studio_text_domain() ) );
@@ -18,21 +29,15 @@
             </span>
 
             <?php } // End if categories ?>
-                                
+
             <?php edit_post_link( __( 'Edit', indie_studio_text_domain() ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
             
-        </div>
-    
-    <?php } else { ?>
-       
-        <h2 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', indie_studio_text_domain() ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h2>
-    
-        <div class="entry-meta group">
+        <?php } else { ?>
+        
             <?php indie_studio_posted_on(); ?>
-        </div>
-    
-    <?php } ?>
-   
+        
+        <?php } ?>
 
-    
+    </div>
+   
 </header>
