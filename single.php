bb28b582ecf06b1646efb094db3038c844dedbfe
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2018-08-04 13:30:10 +0100

    Removed potential error from current build

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 /**
  * The template for showing all single posts.
- * 
+ *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  * @link https://codex.wordpress.org/Post_Type_Templates
  * @link https://www.tsartsaris.gr/how-to-properly-makrup-your-blog-and-blogposts-with-html5-schema-microdata
@@ -18,20 +18,20 @@ get_header(); ?>
 
        <div class="article-inner-wrap">
 
-            <?php 
-           
+            <?php
+
             while ( have_posts() ) : the_post();
-           
+
                 get_template_part( 'template-parts/post/content', get_post_format() );
-           
+
                 // If we want to share article
                 if ( get_theme_mod('indie_studio_social_share', true) ) {
-           
+
                     indie_studio_social_share( get_the_id() );
-                    
+
                 }
-           
-           
+
+
                 // If comments are open or we have at least one comment, show the comment template
                 if ( comments_open() || get_comments_number() != 0 ){
                     comments_template( '', true );
@@ -39,17 +39,17 @@ get_header(); ?>
 
                 /**
                  * @TODO Add attractive post navigation.
-                 **/ 
+                 **/
 
-            endwhile; // end of the loop. 
+            endwhile; // end of the loop.
             ?>
 
         </div>
-        
+
     </main><!-- #content -->
-    
-    <?php get_template_part( 'template-parts/extras/related' ); ?>
-    
+
+    <?php //get_template_part( 'template-parts/extras/related' ); ?>
+
 </section><!-- #primary -->
 
 <?php get_footer();
